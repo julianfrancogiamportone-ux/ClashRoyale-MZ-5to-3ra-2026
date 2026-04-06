92fd6158ce65f3868cda897ffda45229786b731e
unknown <ignaate0707m@gmail.com>
2026-04-06 11:11:02 -0300

    ataque entity

---
+++ src/main/java/clashroyale/entidades/ataque.java
@@ -0,0 +1,42 @@
+package clashroyale.entidades;
+
+import jakarta.persistence.*;
+@Entity
+public class ataque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private int costoEnergia;
+    private int danioBase;
+
+public int getId() {
+    return id;
+}
+public void setId(int Id) {
+    this.id = id;
+}
+public  String getNombre(){
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public int getcostoEnergia() {
+    return costoEnergia;
+}
+public void setcostoEnergia(int costoEnergia){
+    this.costoEnergia = costoEnergia;
+}
+public int getdanioBase() {
+    return danioBase;
+}
+public void setdanioBase(int danioBase){
+    this.danioBase = danioBase;
+}
+}
+    
+
+
+

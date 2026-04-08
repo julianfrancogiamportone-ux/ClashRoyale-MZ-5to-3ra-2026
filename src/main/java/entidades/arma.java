bf245004c0db7fb620370ab2dae214cb7ef62b1b
unknown <ignacioagustinduo@gmail.com>
2026-04-08 10:18:16 -0300

    arma1.1

--- src/main/java/entidades/arma.java
+++ src/main/java/entidades/arma.java
@@ -7,17 +7,17 @@ public class arma {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String nombre;
     private int bonificadorDanio;
     private float peso;
     
     
 
-    public int getId() {
+    public Long getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getNombre() {
